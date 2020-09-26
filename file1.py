c52597047bcb19ce83646917757f07859ff95ded
asfasfds2332 <asfasfds2332@gmail.com>
2020-09-26 18:36:05 +0200

    first commit

---
+++ file1.py
@@ -0,0 +1,16 @@
+#nazwie się później
+#INFORMACJE
+
+############
+#IMPORT
+############
+
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+import numpy as np
+
+############
+#
+############
+
+
