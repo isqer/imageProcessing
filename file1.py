d90555103a0ec02ebea06b75d47a317fa8ddc439
asfasfds2332 <asfasfds2332@gmail.com>
2020-09-26 23:37:34 +0200

    Negative, showImage 1 or 3 canals

--- file1.py
+++ file1.py
@@ -13,30 +13,50 @@ import numpy as np
 #
 ############
 
-#wrzuci się do klasy później - musimy to omówić
+############
+# MISC
+############
 
-def grayscaleNTSC(picture):
-    output=np.zeros((picture.shape[0],picture.shape[1]))
+#wrzuci się do klasy później - musimy to omówić
+def showImage(picture):
+    if picture.ndim==2: plt.imshow(newPic, cmap='gray')
+    else: plt.imshow(newPic, cmap=None) #dzięki temu nie musisz wybierać między cmap gray a None
+
+############
+#
+############
+
+def grayScaleNTSC(picture):
+    output=np.zeros((picture.shape[0],picture.shape[1]))    # czy z tego funkcje?
     for x in range(0,picture.shape[0]):
         for y in range(0,picture.shape[1]):
             output[x,y]=0.299*picture[x,y,0]+0.587*picture[x,y,1]+0.114*picture[x,y,2]
-    plt.imshow(output, cmap='gray')
-    plt.show()
+    return output
 
-def grayscaleHDTV(picture):
-    output=np.zeros((picture.shape[0],picture.shape[1]))
+def grayScaleHDTV(picture):
+    output=np.zeros((picture.shape[0],picture.shape[1]))     # czy z tego funkcje?
     for x in range(0,picture.shape[0]):
         for y in range(0,picture.shape[1]):
             output[x,y]=0.2126*picture[x,y,0]+0.7152*picture[x,y,1]+0.0722*picture[x,y,2]
-    plt.imshow(output, cmap='gray')
-    plt.show()
+    return output
 
-def grayscaleHDR(picture):
-    output=np.zeros((picture.shape[0],picture.shape[1]))
+
+def grayScaleHDR(picture):
+    output=np.zeros((picture.shape[0],picture.shape[1]))    # czy z tego funkcje?
     for x in range(0,picture.shape[0]):
         for y in range(0,picture.shape[1]):
             output[x,y]=0.2627*picture[x,y,0]+0.6780*picture[x,y,1]+0.0593*picture[x,y,2]
-    plt.imshow(output, cmap='gray')
-    plt.show()
+    return output
 
-grayscaleNTSC(img.imread("laura.tif"))
+def negativeFilter(picture):
+    output=0
+    if picture.ndim==2: output=255-picture[:,:]
+    else: output=255-picture[:,:,:]
+    return output
+############
+# TESTS?
+############
+
+newPic=negativeFilter(img.imread("laura.tif"))
+showImage(newPic)
+plt.show()
