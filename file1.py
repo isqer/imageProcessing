0c106712a28d79a55c48a1afbe76f76517861dfd
asfasfds2332 <asfasfds2332@gmail.com>
2020-09-26 19:18:51 +0200

    grayscale filter

--- file1.py
+++ file1.py
@@ -13,4 +13,30 @@ import numpy as np
 #
 ############
 
+#wrzuci się do klasy później - musimy to omówić
 
+def grayscaleNTSC(picture):
+    output=np.zeros((picture.shape[0],picture.shape[1]))
+    for x in range(0,picture.shape[0]):
+        for y in range(0,picture.shape[1]):
+            output[x,y]=0.299*picture[x,y,0]+0.587*picture[x,y,1]+0.114*picture[x,y,2]
+    plt.imshow(output, cmap='gray')
+    plt.show()
+
+def grayscaleHDTV(picture):
+    output=np.zeros((picture.shape[0],picture.shape[1]))
+    for x in range(0,picture.shape[0]):
+        for y in range(0,picture.shape[1]):
+            output[x,y]=0.2126*picture[x,y,0]+0.7152*picture[x,y,1]+0.0722*picture[x,y,2]
+    plt.imshow(output, cmap='gray')
+    plt.show()
+
+def grayscaleHDR(picture):
+    output=np.zeros((picture.shape[0],picture.shape[1]))
+    for x in range(0,picture.shape[0]):
+        for y in range(0,picture.shape[1]):
+            output[x,y]=0.2627*picture[x,y,0]+0.6780*picture[x,y,1]+0.0593*picture[x,y,2]
+    plt.imshow(output, cmap='gray')
+    plt.show()
+
+grayscaleNTSC(img.imread("laura.tif"))
