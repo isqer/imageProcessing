3a30891312c660a7c7f9bcec5f491d3ce6048ee4
asfasfds2332 <asfasfds2332@gmail.com>
2020-09-28 11:21:39 +0200

    wrap in class

--- file1.py
+++ file1.py
@@ -25,38 +25,43 @@ def showImage(picture):
 ############
 #
 ############
+class filters:
+    def __init__(self):
+        pass        #jakis init musi byc
 
-def grayScaleNTSC(picture):
-    output=np.zeros((picture.shape[0],picture.shape[1]))    # czy z tego funkcje?
-    for x in range(0,picture.shape[0]):
-        for y in range(0,picture.shape[1]):
-            output[x,y]=0.299*picture[x,y,0]+0.587*picture[x,y,1]+0.114*picture[x,y,2]
-    return output
+    def grayScaleNTSC(self, picture):   #zawsze musisz dac self
+        output=np.zeros((picture.shape[0],picture.shape[1]))    # czy z tego funkcje?
+        for x in range(0,picture.shape[0]):
+            for y in range(0,picture.shape[1]):
+                output[x,y]=0.299*picture[x,y,0]+0.587*picture[x,y,1]+0.114*picture[x,y,2]
+        return output
 
-def grayScaleHDTV(picture):
-    output=np.zeros((picture.shape[0],picture.shape[1]))     # czy z tego funkcje?
-    for x in range(0,picture.shape[0]):
-        for y in range(0,picture.shape[1]):
-            output[x,y]=0.2126*picture[x,y,0]+0.7152*picture[x,y,1]+0.0722*picture[x,y,2]
-    return output
+    def grayScaleHDTV(self, picture):
+        output=np.zeros((picture.shape[0],picture.shape[1]))     # czy z tego funkcje?
+        for x in range(0,picture.shape[0]):
+            for y in range(0,picture.shape[1]):
+                output[x,y]=0.2126*picture[x,y,0]+0.7152*picture[x,y,1]+0.0722*picture[x,y,2]
+        return output
 
 
-def grayScaleHDR(picture):
-    output=np.zeros((picture.shape[0],picture.shape[1]))    # czy z tego funkcje?
-    for x in range(0,picture.shape[0]):
-        for y in range(0,picture.shape[1]):
-            output[x,y]=0.2627*picture[x,y,0]+0.6780*picture[x,y,1]+0.0593*picture[x,y,2]
-    return output
+    def grayScaleHDR(self, picture):
+        output=np.zeros((picture.shape[0],picture.shape[1]))    # czy z tego funkcje?
+        for x in range(0,picture.shape[0]):
+            for y in range(0,picture.shape[1]):
+                output[x,y]=0.2627*picture[x,y,0]+0.6780*picture[x,y,1]+0.0593*picture[x,y,2]
+        return output
 
-def negativeFilter(picture):
-    output=0
-    if picture.ndim==2: output=255-picture[:,:]
-    else: output=255-picture[:,:,:]
-    return output
+    def negativeFilter(self, picture):
+        output=0
+        if picture.ndim==2: output=255-picture[:,:]
+        else: output=255-picture[:,:,:]
+        return output
 ############
 # TESTS?
 ############
 
-newPic=negativeFilter(img.imread("laura.tif"))
+
+flt = filters()
+newPic=flt.negativeFilter(img.imread("laura.tif"))
 showImage(newPic)
 plt.show()
