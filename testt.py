09ca4067dd1c61d9b93b208de459cd539fcc6ce7
user <isqer97@gmail.com>
2020-10-03 19:21:53 +0200

    update

---
+++ testt.py
@@ -0,0 +1,19 @@
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+import numpy as np
+
+# ############
+# # TESTS?
+# ############
+#
+# sv=FileFormat()
+# flt = Filters()
+# ops = Operations()
+# #newPic=ops.flipImageVertical(flt.negativeFilter(flt.grayScaleHDR(img.imread("laura.tif"))))
+# newPic=flt.contrast((img.imread("laura.tif")),128)
+# print(np.min(newPic))
+# showImage(newPic)
+# plt.show()
+# #sv.saveAsBMP(img.imread("laura.tif"), 'test.bmp')
+
+
